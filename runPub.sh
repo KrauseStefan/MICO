8cfc178a988adfda0b91a09bcda12d4923dcd2c2
Stefan Krause-Kjaer <stefan.krausekjaer@gmail.com>
2013-11-17 19:25:19 +0100

    added run scripts

---
+++ runPub.sh
@@ -0,0 +1,1 @@
+java -cp /usr/local/RTI/ndds.5.0.0/class/nddsjava.jar:./Publiher/out/production/Publiher/ com.IHA.MICO.simple.Main
---
+++ runSub.sh
@@ -0,0 +1,1 @@
+java -cp /usr/local/RTI/ndds.5.0.0/class/nddsjava.jar:./Subscriber/out/production/Subscriber/ com.IHA.MICO.simple.Main
