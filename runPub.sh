1b6e26981ffee89d81c58db028b1ecc2fdb7f504
Stefan Krause-Kjaer <stefan.krausekjaer@gmail.com>
2013-11-17 20:38:36 +0100

    Removed static paths

--- runPub.sh
+++ runPub.sh
@@ -1,1 +1,1 @@
-java -cp /usr/local/RTI/ndds.5.0.0/class/nddsjava.jar:./Publiher/out/production/Publiher/ com.IHA.MICO.simple.Main
+java -cp $NDDSHOME/class/nddsjava.jar:./Publiher/out/production/Publiher/ com.IHA.MICO.simple.Main
--- runSub.sh
+++ runSub.sh
@@ -1,1 +1,1 @@
-java -cp /usr/local/RTI/ndds.5.0.0/class/nddsjava.jar:./Subscriber/out/production/Subscriber/ com.IHA.MICO.simple.Main
+java -cp $NDDSHOME/class/nddsjava.jar:./Subscriber/out/production/Subscriber/ com.IHA.MICO.simple.Main
